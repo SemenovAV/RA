d0410a69f29059f01609118a85bf62be5a9b9de5
Aleksey Semenov <a.s.php@yandex.ru>
2018-11-14 00:55:43 +0300

    Функциональные компоненты.
    Выполнил второе задание(chat)

--- func-component/chat/js/MessageHistory.js
+++ func-component/chat/js/MessageHistory.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const MessageHistory =({list}) =>{
+  const isFunction = input =>typeof input === 'function';
+  const renderIf = predicate => elemOrThunk => predicate ?(isFunction(elemOrThunk) ? elemOrThunk(): elemOrThunk ) :null;
+  return renderIf(list.length > 0)(<ul>{
+    list.map((message)=>
+      renderIf(message.type === 'message')(<Message message={message} from={message.from}/>) ||
+      renderIf(message.type === 'response')(<Response message={message} from={message.from}/>) ||
+      renderIf (message.type === 'typing')(<Typing message={message} from={message.from}/>))
+  }</ul>)
+}
+
+MessageHistory.defaultProps = {
+  list: [],
+}
