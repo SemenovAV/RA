764739b266944201836552e4afb921f5bed41826
Aleksey Semenov <a.s.php@yandex.ru>
2018-11-13 23:26:50 +0300

    Функциональные компоненты.
    Решил первую задачу.(films)

--- func-component/films/js/Stars.js
+++ func-component/films/js/Stars.js
@@ -1,5 +1,17 @@
 'use strict';
 
-function Stars() {
-  return <ul className="card-body-stars u-clearfix"><li><Star /></li></ul>;
+function Stars({ count }) {
+  return (
+    <ul className="card-body-stars u-clearfix">
+      {
+    (typeof (count) === 'number')
+    && (count => 1 && count <= 5)
+      ? Array(count).fill(<li><Star /></li>) : null
+  }
+    </ul>
+  );
 }
+
+Stars.defaultProps = {
+  count: 0,
+};
