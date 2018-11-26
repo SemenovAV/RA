4651b9451c6b1fc8b8a52309600b7d78c3496b28
Aleksey Semenov <a.s.php@yandex.ru>
2018-11-27 01:44:28 +0300

    Контролируемые компоненты.
    Выполнил задачу 2(library)

--- ctrl-component/library/js/SearchBox.js
+++ ctrl-component/library/js/SearchBox.js
@@ -1,5 +1,16 @@
-const SearchBox = () => {
-    return (
-        <input type="text" placeholder="Поиск по названию или автору"/>
-    );
+class SearchBox extends React.Component {
+    constructor(props){
+        super(props)
+    }
+    render(){
+      return (
+        <input
+          type="text"
+          placeholder="Поиск по названию или автору"
+          ref={el => this.element = el}
+          value={this.props.value}
+          onChange={()=> this.props.filterBooks(this.element.value)}
+        />
+      );
+    }
 };
