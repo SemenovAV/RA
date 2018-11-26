fe39004e90eefaddc7cafdd105fd79820cedbdfa
Aleksey Semenov <a.s.php@yandex.ru>
2018-11-26 10:39:18 +0300

    Композиция компонентов.
    Выполнил задание 1(Listing)

--- composition/listing/js/App.js
+++ composition/listing/js/App.js
@@ -1,16 +1,7 @@
 'use strict';
 
-const App = ({items}) => (
+const App = ({items,colors}) => (
   <main>
-    {items.map(item => {
-      switch(item.type) {
-        case 'unisex':
-          return <Item color="black" item={item} />;
-        case 'male':
-          return <Item color="blue" item={item} />;
-        case 'female':
-          return <Item color="orange" item={item} />;
-      }
-    })}
+    {items.map(item => <Item color={colors[item.type]} item={item} />)}
   </main>
 );
