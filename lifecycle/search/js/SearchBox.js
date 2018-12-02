7a14a92dd8770a0d7a00b6980bcbd2aca56fd14c
Aleksey Semenov <a.s.php@yandex.ru>
2018-12-02 22:29:36 +0300

    Жизненный цикл компонентов.
    Выполнил задачу 1(search)

--- lifecycle/search/js/SearchBox.js
+++ lifecycle/search/js/SearchBox.js
@@ -1,19 +1,35 @@
 class SearchBox extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = { fixed: false };
+  constructor (props) {
+    super(props)
+    this.state = {fixed: false}
+
   }
 
-  render() {
-    return <SearchBoxView fixed={this.state.fixed} />
+  render () {
+    return <SearchBoxView fixed={this.state.fixed}/>
   }
 
-  isFixed() {
-    return undefined;
+  isFixed (e) {
+    this.root = this.root || e.currentTarget.document.querySelector(':root');
+    this.element = this.element || e.currentTarget.document.querySelector('.search-box');
+    this.startPoint = this.startPoint || this.element.offsetTop;
+    return this.root.getBoundingClientRect().y < ~this.startPoint + 1;
   }
 
-  setPosition() {
-    return undefined;
+  setPosition (e) {
+    this.setState({
+      fixed: this.isFixed(e),
+    })
+    return undefined
+  }
+
+  componentDidMount () {
+    this.listener = e => this.setPosition(e)
+    window.addEventListener('scroll', this.listener)
+  }
+
+  componentWillUnmount () {
+    window.removeEventListener('scroll', this.listener)
   }
 }
