34637b902816b3bf5c52fbdb6db3cda864f751e9
Aleksey Semenov <a.s.php@yandex.ru>
2018-12-04 07:54:33 +0300

    Жизненный цикл компонентов.
    Выполнил задачу 3(shop)

--- lifecycle/shop/js/Cart.js
+++ lifecycle/shop/js/Cart.js
@@ -1,9 +1,27 @@
 class Cart extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      update: false,
+    }
+  }
 
-  render() {
+  componentWillReceiveProps ({items, isOpen}) {
+    this.setState({
+      update: this.props.isOpen !== isOpen || isOpen &&
+        this.props.items.length !== items.length,
+    })
+
+  }
+
+  shouldComponentUpdate (nextProps, nextState) {
+    return this.props.isOpen || nextState.update
+  }
+
+  render () {
     return (
       <CartView {...this.props} />
-    );
+    )
   }
 
 }
