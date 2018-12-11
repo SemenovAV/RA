65fd24435dbfff314a22d8d8a4ded97132a83566
Aleksey Semenov <a.s.php@yandex.ru>
2018-12-11 07:51:56 +0300

    Роутинг и XHR
    Выполнил задачу 3(latin)

--- routing-xhr/latin/js/Header.js
+++ routing-xhr/latin/js/Header.js
@@ -1,10 +1,15 @@
-const HeaderComponent = ({ match }) => (
-  <nav className="navbar navbar-light bg-light">
-    {match && match.params.id
-      ? <p className="navbar-brand">Уникальный идентификатор статьи: { match.params.id}</p>
-      : <p className="navbar-brand">Статья не выбрана</p> 
-    }
-  </nav>
-);
+ const {withRouter} = ReactRouterDOM
+const HeaderComponent = ({ location }) => {
+  const articleId = (location)?location.pathname.match(/^\/article\/(\d+)\/?$/i):null;
+  return (
+
+    <nav className="navbar navbar-light bg-light">
+      {articleId
+        ? <p className="navbar-brand">Уникальный идентификатор статьи: { articleId[1]}</p>
+        : <p className="navbar-brand">Статья не выбрана</p>
+      }
+    </nav>
+  );
+}
 
 const Header = withRouter(HeaderComponent);
